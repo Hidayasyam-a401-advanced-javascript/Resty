0f09e133ae06cd085fd09e38986dddc630239d0f
HidayaSyam <hid.sy1910@gmail.com>
2020-10-13 00:31:22 +0300

    Updates

---
+++ precache-manifest.695d75bcfb80953aea3e363d4b27fccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e960ac19c30bca330d3d4ca90ade3206",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "e3ce7b069816a6fe0668",
+    "url": "/resty/static/css/main.6df597dc.chunk.css"
+  },
+  {
+    "revision": "7ad7eb7f6ff66271864d",
+    "url": "/resty/static/js/2.236d22a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.236d22a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ce7b069816a6fe0668",
+    "url": "/resty/static/js/main.199ca68e.chunk.js"
+  },
+  {
+    "revision": "6972438e44fdcad64bce",
+    "url": "/resty/static/js/runtime-main.b658189f.js"
+  }
+]);
